a989b6e1d40dcd1b80d09484c73939382df557ed
Jake Goritski <jgoritski@gmail.com>
2020-01-06 17:13:12 -0500

    basic viewset and serializer impl

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from tracker.models import MoodRecord
+from rest_framework import serializers, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+
+class MoodRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MoodRecord
+        fields = ["user", "score", "description", "created_at", "updated_at"]
+    
+class MoodRecordViewSet(viewsets.ModelViewSet):
+
+    def list(self, request):
+        queryset = request.current_user.moodrecord_set.all()
+        serializer = MoodRecordSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = request.current_user.moodrecord_set.all()
+        try:
+            record = queryset.get()
+        except MoodRecord.DoesNotExist:
+            return Response({"message": "Not found."}, status=404)
+        serializer = MoodRecordSerializer(record)
+        return Response(serializer.data)
